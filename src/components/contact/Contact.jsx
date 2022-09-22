393ba2db1e5819405ec3a89795e498909b0bbbf4
Watt <wattjer@b0f1d85820fa.ant.amazon.com>
2022-09-22 15:18:02 -0700

    first up

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import { Message } from "@material-ui/icons";
+import React, { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    setMessage(true);
+    e.preventDefault();
+  };
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2> Contact </h2>
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="email" />
+          <textarea placeholder="message"></textarea>
+          <button type="submit">Submit</button>
+          {message && <span>Thanks! I will reply ASAP.</span>}
+        </form>
+      </div>
     </div>
   );
 }
